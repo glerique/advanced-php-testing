7fa98c41d545584656c59d978c16a0b5cacaf961
glerique <gael.lerique@gmail.com>
2025-07-05 15:05:52 +0200

    refactor(tests): simplify CartTest by using class property for Cart instance

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -1,19 +1,35 @@
 <?php
 
+namespace App\Tests;
+
 use App\Cart;
 use PHPUnit\Framework\TestCase;
 
 class CartTest extends TestCase
 {
+    private Cart $cart;
+
+    protected function setUp(): void
+    {
+        $this->cart = new Cart();
+    }
+    
     public function testNetPriceIsCalculatedCorrectly(): void
     {
-        
-        $cart = new Cart();
-        $cart->price = 10;
+        $this->cart->price = 10;
 
-       
-        $netPrice = $cart->getNetPrice();
+        $netPrice = $this->cart->getNetPrice();
 
         $this->assertEquals(12, $netPrice);
+    }
+
+    public function testTheCartTaxValueCanBeChangedStatically(): void
+    {
+        $this->cart->price = 10;
+
+        $this->cart->setTax(1.5);
+        $netPrice = $this->cart->getNetPrice();
+
+        $this->assertEquals(15, $netPrice);
     }
 }
