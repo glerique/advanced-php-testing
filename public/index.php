58ff0431f3a378f2b60e0e9de835d58971ea6486
glerique <gael.lerique@gmail.com>
2025-07-07 10:32:21 +0200

    feat(demo): add example usage of Cart with TaxCalculator

--- public/index.php
+++ public/index.php
@@ -1,2 +1,10 @@
 <?php
-echo "Hello, World !\n";
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\Cart;
+use App\Service\TaxCalculator;
+
+
+$cart = new Cart(new TaxCalculator(1.2));
+$cart->price = 10;
+echo $cart->getNetPrice();
