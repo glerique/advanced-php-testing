96ba9be4098cef6ca9a5661f5806aac39a45e59e
glerique <gael.lerique@gmail.com>
2025-07-07 10:32:54 +0200

    test(cart): update tests to use TaxCalculator mock

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -3,33 +3,35 @@
 namespace App\Tests;
 
 use App\Cart;
+use App\Service\TaxCalculatorInterface;
 use PHPUnit\Framework\TestCase;
 
 class CartTest extends TestCase
 {
+    private $taxCalculator;
     private Cart $cart;
 
     protected function setUp(): void
     {
-        $this->cart = new Cart();
+        $this->taxCalculator = $this->createMock(TaxCalculatorInterface::class);
+        $this->cart = new Cart($this->taxCalculator);
     }
-    
+
     public function testNetPriceIsCalculatedCorrectly(): void
     {
+        
+        $this->taxCalculator->method('calculate')->willReturn(12.0);
         $this->cart->price = 10;
 
-        $netPrice = $this->cart->getNetPrice();
-
-        $this->assertEquals(12, $netPrice);
+        $this->assertEquals(12, $this->cart->getNetPrice());
     }
 
-    public function testTheCartTaxValueCanBeChangedStatically(): void
+    public function testNetPriceWithDifferentTaxRate(): void
     {
+        
+        $this->taxCalculator->method('calculate')->willReturn(15.0);
         $this->cart->price = 10;
 
-        $this->cart->setTax(1.5);
-        $netPrice = $this->cart->getNetPrice();
-
-        $this->assertEquals(15, $netPrice);
+        $this->assertEquals(15, $this->cart->getNetPrice());
     }
 }
